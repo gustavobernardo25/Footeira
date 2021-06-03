604a939e516fd2cdf42665c422cfb06d72cc180d
Gustavo Bernardo <gustavobernardobas@gmail.com>
2021-06-03 20:30:19 -0300

    Segundo Commit

---
+++ jogador.js
@@ -0,0 +1,88 @@
+function searchPlayer(){
+    document.getElementById('container').innerHTML = '';
+   const playerName = document.getElementById('search').value;
+   fetch(` https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${playerName}`).then(res => res.json()).then(data =>{
+console.log(data)
+
+       for (item of data.player){
+           console.log(item)
+           var nome = document.createElement('p');
+           var time = document.createElement('p');
+           var fotoJogador = document.createElement('img');
+           var idJogador = document.createElement('p');
+
+           nome.append(item.strPlayer);
+           time.append(item.strTeam);
+           fotoJogador.setAttribute('src', item.strCutout);
+           idJogador.append(item.idPlayer);
+
+
+      const div = document.createElement('div');
+      div.setAttribute('class', 'Content1');
+      div.setAttribute('onclick','playerSelec()');
+     
+
+      div.append(fotoJogador,nome,time,idJogador);
+      document.getElementById('container').append(div)
+
+    }
+})
+
+}
+
+function playerSelec(){
+    const idJogador = document.getElementById('container').firstChild.lastChild.innerText
+    document.getElementById('container').innerHTML = '';
+
+
+    fetch(`https://www.thesportsdb.com/api/v1/json/1/lookupplayer.php?id=${idJogador}`).then(res => res.json()).then(data =>{
+        
+        for (item of data.player){
+        console.log(item)
+        var nome = document.createElement('p');
+        var fotoJogador = document.createElement('img');
+        var img1 = document.createElement('img');
+        var img2 = document.createElement('img');
+        var linkInsta = document.createElement('a');
+        var linkFace = document.createElement('a');
+        var linkTwitter = document.createElement('a');
+        var altura = document.createElement('p');
+        var peso = document.createElement('p');
+        var posicao = document.createElement('p');
+        var dscJogador = document.createElement('p');
+        var local = document.createElement('p');
+        var chuteira = document.createElement('iframe')
+        var data = document.createElement('p');
+        var nacionalidade = document.createElement('p');
+        var time = document.createElement('p');
+
+        
+        nome.append(item.strPlayer);
+        fotoJogador.setAttribute('src',item.strCutout);
+        img1.setAttribute('src',item.strThumb)
+        img2.setAttribute('src',item.strRender)
+        linkInsta.setAttribute('src','https://' + item.strInstagram);
+        linkInsta.append('https://' + item.strInstagram);
+        linkFace.setAttribute('src','https://' + item.strFacebook);
+        linkFace.append('https://' + item.strFacebook);
+        linkTwitter.setAttribute('src','https://' + item.strTwitter);
+        linkTwitter.append('https://' + item.strTwitter);
+        altura.append(item.strHeight);
+        peso.append(item.strWeight);
+        posicao.append(item.strPosition);
+        dscJogador.append(item.strDescriptionEN);
+        local.append(item.strBirthLocation);
+        chuteira.setAttribute('src', `https://www.zoom.com.br/search?q=${item.strKit}`);
+        data.append(item.dateBorn);
+        nacionalidade.append(item.strNationality);
+        time.append(item.strTeam);
+    
+
+
+        const div = document.createElement('div');
+        div.setAttribute('class','Content2')
+        div.append(nome,fotoJogador,img1,img2,linkInsta,linkFace,linkTwitter,altura,peso,posicao,dscJogador,local,chuteira,data,nacionalidade,time);
+        document.getElementById('container').append(div)
+        }
+    })
+}   
--- script.js
+++ script.js
@@ -17,18 +17,69 @@ function searchTeam(){
            escudo.setAttribute('src', item.strTeamBadge);
            id.append(item.idTeam);
 
+
       const div = document.createElement('div');
-      div.setAttribute('class', 'teamInfo');
+      div.setAttribute('class', 'Content1');
       div.setAttribute('onclick','teamSelec()');
-      id.setAttribute('id', 'id');
+     
 
       div.append(escudo, nome, esporte, liga, id);
       document.getElementById('container').append(div)
 
-       }
-   })
+    }
+})
 
 }
-function teamSelec(){
 
+function teamSelec(){
+    const idDoTime = document.getElementById('container').firstChild.lastChild.innerText
+    console.log(idDoTime)
+    document.getElementById('container').innerHTML = '';
+
+    fetch(`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idDoTime}`).then(res => res.json()).then(data =>{
+        
+        for (item of data.teams){
+        console.log(item)
+        var nome = document.createElement('p');
+        var liga = document.createElement('p');
+        var esporte = document.createElement('p');
+        var dsctime = document.createElement('p');
+        var estadio = document.createElement('p');
+        var imgestadio = document.createElement('img')
+        var dscestadio = document.createElement('p');
+        var capacidade = document.createElement('p');
+        var cidade = document.createElement('p');
+        var pais = document.createElement('p')
+        var escudo = document.createElement('img');
+        var id = document.createElement('p');
+        var img1 = document.createElement('img');
+        var img2 = document.createElement('img');
+        var link = document.createElement('a');
+        
+        nome.append(item.strTeam);
+        liga.append(item.strLeague);
+        esporte.append(item.strSport);
+        dsctime.append(item.strDescriptionEN);
+        estadio.append(item.str);
+        imgestadio.setAttribute('src',item.strStadiumThumb);
+        capacidade.append(item.intStadiumCapacity);
+        pais.append(item.strCountry);
+        cidade.append(item.strStadiumLocation);
+        dscestadio.append(item.strStadiumDescription);
+        escudo.setAttribute('src', item.strTeamBadge);
+        img1.setAttribute('src',item.strTeamJersey)
+        img2.setAttribute('src',item.strTeamLogo)
+        id.append(item.idTeam);
+        link.setAttribute('src','https://' + item.strYoutube);
+        link.append('https://' + item.strYoutube);
+
+
+        const div = document.createElement('div');
+        div.setAttribute('class','Content2')
+        div.append(escudo, nome, esporte, liga, id,dsctime,imgestadio,dscestadio,capacidade,cidade,pais,img1,img2,link);
+        document.getElementById('container').append(div)
+        }
+    })
+
+    
 }
